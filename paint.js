bea88bac0e9582d1e41d6c9496191381720da073
Axel Björnsson <axelbjornsson@gmail.com>
2017-01-19 16:17:03 +0000

    Shiiiiiit

--- paint.js
+++ paint.js
@@ -1,53 +1,71 @@
-$(document).ready(function(){
+function getMousePos(canvas, e) {
+    var clientrect = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - clientrect.left,
+        y: e.clientY - clientrect.top
+    };
+}
+
+$(document).ready(function () {
     var canvas = document.getElementById("whiteboard");
     var context = canvas.getContext("2d");
+    var tool = document.getElementById("drawingTools")
     var startX = 0;
     var startY = 0;
     var isDrawing = false;
+    
 
-    $("#whiteboard").mousedown(function(e){
-        var x = e.pageX - this.offsetLeft;
-        var y = e.pageY - this.offsetTop;
+    $(canvas).mousedown(function (e) {
+        var p = getMousePos(canvas, e);
+        var x = p.x;
+        var y = p.y;
 
         startX = x;
         startY = y;
         isDrawing = true;
-
-        // console.log("X: "+  x+ "Y:" + y);
-        // context.fillStyle = "green";
-        // context.fillRect(x-30,y-30,60,60);
-        // context.strokeRect(x-30,y-30,60,60);
-
+        
     });
-    $("whiteboard").mouse
-    $("#whiteboard").mousemove(function (e) {
-        if(isDrawing === true){
-            var x = e.pageX - this.offsetLeft;
-            var y = e.pageY - this.offsetTop;
-            context.beginPath();
-            context.moveTo(100,100);
-            context.lineTo(100,120);
-            context.lineTo(120,120);
-            context.lineTo(120,140);
-            context.lineTo(100,140);
-            context.lineTo(100,160);
-            context.lineTo(80,160);
-            context.lineTo(80,140);
-            context.lineTo(60,140);
-            context.lineTo(60,120);
-            context.lineTo(80,120);
-            context.lineTo(80,100);
-            context.lineTo(100,100);
-            context.stroke();
-            // context.beginPath();
-            // context.moveTo(startX,startY);
-            // context.lineTo(x,y);
-            // context.stroke();
+
+    $(canvas).mousemove(function (e) {
+        if (isDrawing === true) {
+            var p = getMousePos(canvas, e);
+            var x = p.x;
+            var y = p.y;
+            // DRAW PEN
+            if (tool.options[tool.selectedIndex].value === "pen") {
+                // TODO: implement pen
+            }
+            // DRAW LINE 
+            else if (tool.options[tool.selectedIndex].value === "line") {
+                // TODO: 
+                context.beginPath();
+                context.moveTo(startX, startY);
+                context.lineTo(x, y);
+                context.stroke();
+            } 
+            // DRAW RECTANGLE
+            else if (tool.options[tool.selectedIndex].value === "rectangle") {
+                // TODO: 
+                console.log("X: " + x + "Y:" + y);
+                context.fillStyle = "green";
+                context.strokeRect(x, y, startX-x, startY-y);
+            }
+            // DRAW CIRCLE
+            else if (tool.options[tool.selectedIndex].value === "circle") {
+                // TODO: 
+                console.log("X: " + x + "Y:" + y);
+                context.fillStyle = "red";
+                context.strokeRect(x, y, startX-x, startY-y);
+            } 
+            // DRAW TEXT
+            else if(tool.options[tool.selectedIndex.value] === "text") {
+                
+            }
         }
 
     });
 
-    $("#whiteboard").mouseup(function (e) {
+    $(canvas).mouseup(function (e) {
         isDrawing = false;
     })
 });
