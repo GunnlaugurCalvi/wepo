364bcbe6b9f394bf5eb99bf43347bdb446b59aed
Axel Björnsson <axelbjornsson@gmail.com>
2017-01-24 16:34:35 +0000

    JS

--- paint.js
+++ paint.js
@@ -1,5 +1,3 @@
-var Shapes = require("./shape");
-
 function getMousePos(canvas, e) {
     var clientrect = canvas.getBoundingClientRect();
     return {
@@ -8,9 +6,8 @@ function getMousePos(canvas, e) {
     };
 }
 
-
-
-$(document).ready(function() {
+$(document).ready(function () {
+    var tool = document.getElementById("drawingTools")
     var Settings = {
         canvas: document.getElementById("whiteboard"),
         cShape: tool.options[tool.selectedIndex].value,
@@ -20,30 +17,34 @@ $(document).ready(function() {
         shapes: []
     }
     var context = Settings.canvas.getContext("2d");
-    var tool = document.getElementById("drawingTools")
-    
-    var startp = {x:0, y:0};
+
+    var startp = {
+        x: 0,
+        y: 0
+    };
 
     $(Settings.canvas).mousedown(function (e) {
         var shape = undefined;
-        
+        Settings.cShape = tool.options[tool.selectedIndex].value;
+
         var p = getMousePos(Settings.canvas, e);
         startp = p;
-        if(!Settings.isDrawing) {
+        if (!Settings.isDrawing) {
             Settings.isDrawing = true;
         }
 
-        if(Settings.cShape === "pen") {
+        if (Settings.cShape === "pen") {
             console.log("PEN");
-        } else if(Settings.cShape === "line") {
-            shape = Shapes.Line(startp.x, startp.y, Settings.color);
-        } else if(Settings.cShape === "rectangle") {
-            shape = Shapes.Rectangle(startp.x, startp.y, Settings.color);
-        } else if(Settings.cShape === "circle") {
-            shape = Shapes.Circle(startp.x, startp.y, Settings.color);
-        } else if(Settings.cShape === "text") {
+        } else if (Settings.cShape === "line") {
+            shape = Shape.Line(startp.x, startp.y, Settings.color);
+        } else if (Settings.cShape === "rectangle") {
+            shape = Shape.Rectangle(startp.x, startp.y, Settings.color);
+        } else if (Settings.cShape === "circle") {
+            shape = Shape.Circle(startp.x, startp.y, Settings.color);
+        } else if (Settings.cShape === "text") {
             console.log("TEXT");
         }
+        
         console.log(shape);
     });
 
@@ -64,28 +65,28 @@ $(document).ready(function() {
                 // context.moveTo(startp.x, startp.y);
                 // context.lineTo(p.x, p.y);
                 // context.stroke();
-            } 
+            }
             // DRAW RECTANGLE
             else if (Settings.cShape === "rectangle") {
                 // TODO: 
                 context.fillStyle = "green";
-                context.strokeRect(p.x, p.y, startp.x-p.x, startp.y-p.y);
+                context.strokeRect(p.x, p.y, startp.x - p.x, startp.y - p.y);
             }
             // DRAW CIRCLE
             else if (Settings.cShape === "circle") {
                 // TODO: 
                 context.fillStyle = "red";
-                context.strokeRect(p.x, p.y, startp.x-p.x, startp.y-p.y);
-            } 
+                context.strokeRect(p.x, p.y, startp.x - p.x, startp.y - p.y);
+            }
             // DRAW TEXT
-            else if(Settings.cShape === "text") {
-                
+            else if (Settings.cShape === "text") {
+
             }
         }
 
     });
 
-    $(canvas).mouseup(function (e) {
-        isDrawing = false;
+    $(Settings.canvas).mouseup(function (e) {
+        Settings.isDrawing = false;
     })
 });
